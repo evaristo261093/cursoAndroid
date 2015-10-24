0dc1940bbfa811125ae579d0c0e93abcc00e18bd
evaristo261093 <lcmaxidesing@gmail.com>
2015-10-24 10:32:17 -0500

    Ejemplo0001 del curso de android
    solo es la presentacion de un msg de bienvenida

---
+++ Ejemplo001/main/java/com/example/evaristo/ejemplo001/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.evaristo.ejemplo001;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Button btn_send = (Button) findViewById(R.id.btn_enviar);
+        btn_send.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"hizo Click",Toast.LENGTH_SHORT).show();
+
+                EditText edit_name = (EditText) findViewById(R.id.edit_name);
+                TextView TextView = (TextView) findViewById(R.id.textView);
+                TextView.setText("Hola " + edit_name.getText());
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
